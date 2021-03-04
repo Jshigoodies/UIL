70ba660d3595d8f85faa9cd14f320e3593f43126
unknown <jshigoodies@gmail.com>
2021-03-03 19:28:10 -0800

    UIL

---
+++ Confusing/src/MainFive.java
@@ -0,0 +1,21 @@
+
+public class MainFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.BYTES + " " );
+		System.out.println(Integer.SIZE);
+		System.out.println(Byte.BYTES);
+		//Bits divide by 8 = Bytes
+		
+		
+		
+		//public static double cbrt(double a);
+		//Cube Root
+		
+		
+		
+		
+	}
+
+}
---
+++ Confusing/src/MainSix.java
@@ -0,0 +1,10 @@
+
+public class MainSix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+		System.out.println();
+	}
+
+}
