700e293a2c26d8377e6a7570b19079109e4dcf61
unknown <jshigoodies@gmail.com>
2021-03-07 11:16:33 -0800

    UIL

--- Confusing/src/MainSix.java
+++ Confusing/src/MainSix.java
@@ -1,10 +1,91 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainSix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random r = new Random();
-		System.out.println();
+		System.out.println((int)r.nextInt(4));
+		//number from 0 to 4
+		//4 is exclusive
+		//Cannot put negative numbers in nextInt(int number)
+		
+		
+		
+		
+		//String str;
+		//System.out.println(str);
+		//Syntax error
+		//VariableNotInitialized does not exist
+		
+		
+		
+		
+		//Look at Main Three for regex
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		int arr             [] = new int[5];
+		int n = 1, sum = 0;
+		for(int i = 0; i <5; i++)
+		{
+			arr[i] = n
+		}
+		
+		*/
+		
+		
+		
+		
+		
+		//Question 23 - ORDER ACTUALLY MATTERS
+		
+		
+		
+		
+		// \n is considered a space aka \s
+		
+		
+		
+		/*
+		Integer num = new Integer(10);
+		double num1 = new Integer(10);
+		int num2 = new Integer(10);
+		int num3 = new Boolean(true);
+		int num4 = new Double(10);
+		Autoboxing = to object
+		Unboxing = from object
+		*/
+		
+		
+		
+		
+		
+		
+		//static int i;
+		//System.out.println(i++);
+		
+		//Only when a variable is a instance variable in a class, it has a default value automatcially even if you didn't assign anything
+		
+		
+		
+		String str1 = "test";
+		int i = 0; //it can be declared outside the loop
+		for(; true;)
+		{
+			i++; //same thing for the increment
+			System.out.println(i);
+		}
+		
+		
+		
 	}
 
 }
