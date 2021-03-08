cd45f2c6fb6eff2e26ad766f4c4142c1aae80f45
Jason Shi <0001074369@ccisd.net>
2021-03-08 11:34:16 -0600

    UIL

---
+++ Confusing/src/MainSeven.java
@@ -0,0 +1,15 @@
+
+public class MainSeven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'A';
+		int i = 8;
+		double d = 4.65;
+		System.out.println(c+i-d); //65 + 8 - 4.65 = 68.35
+		
+		
+		System.out.println(3+5*6-1); //just wanted to make sure order of operations still apply here.
+	}
+
+}
