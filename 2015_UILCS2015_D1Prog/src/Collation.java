85bcaa5b41d46b312c84e3337e7bfccdc42a3bf9
unknown <jshigoodies@gmail.com>
2021-03-17 20:37:06 -0700

    UIL

---
+++ 2015_UILCS2015_D1Prog/src/Collation.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Collation {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		File f = new File("collation.dat");
+		Scanner input = new Scanner(f);
+		
+		//kylographical order of the alphabet
+		//Can't fking find what it means on the interent. First few links were Twitter tweets.
+		
+		input.close();
+	}
+
+}
